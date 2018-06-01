4a82c4306852302b99763b31f487082f56ca6f20
John Byrne <john.l.byrne@hpe.com>
2018-05-31 18:26:15 -0700

    prov/zhpe: Add parameters to allow backend provider/domain to be specified for libfabric backend.
    Signed-off-by: John Byrne <john.l.byrne@hpe.com>

--- prov/zhpe/include/zhpe.h
+++ prov/zhpe/include/zhpe.h
@@ -323,6 +323,8 @@ extern int zhpe_av_def_sz;
 extern int zhpe_cq_def_sz;
 extern int zhpe_eq_def_sz;
 extern char *zhpe_pe_affinity_str;
+extern char *zhpe_fab_backend_prov;
+extern char *zhpe_fab_backend_dom;
 extern int zhpe_ep_max_eager_sz;
 
 static inline void *zhpe_mremap(void *old_address, size_t old_size,
