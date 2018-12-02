480d41355148a45884ac2895d2b5dff50ca2a1e6
John Byrne <john.l.byrne@hpe.com>
2018-12-01 22:13:44 -0800

    Add memreg debug logging
    Signed-off-by: John Byrne <john.l.byrne@hpe.com>

--- prov/zhpe/src/zhpe_dom.c
+++ prov/zhpe/src/zhpe_dom.c
@@ -284,6 +284,8 @@ int zhpe_zmr_reg(struct zhpe_domain *domain, const void *buf,
 	zmr->zkey.internal = !!(qaccess & ZHPE_MR_KEY_INT);
 
 	ret = zhpeq_mr_reg(domain->zdom, buf, len, qaccess, &zmr->kdata);
+	ZHPE_LOG_DBG("dom %p buf %p len 0x%lx qa 0x%x key 0x%lx/%d ret %d\n",
+		     domain, buf, len, qaccess, key, zmr->zkey.internal, ret);
 	if (ret < 0)
 		goto done;
 
