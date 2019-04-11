f95b3f9dfcdf8eb06f20319e0f5a3605bd1f638c
John Byrne <john.l.byrne@hpe.com>
2019-04-11 09:18:10 -0700

    Fix typo I just checked in
    Signed-off-by: John Byrne <john.l.byrne@hpe.com>

--- prov/zhpe/src/zhpe_progress.c
+++ prov/zhpe/src/zhpe_progress.c
@@ -1320,10 +1320,9 @@ static inline void zhpe_pe_progress_rx_queue(struct zhpe_tx *ztx)
 				break;
 
 			case ZHPE_OP_SEND:
-				zhpe_stats_start(
-					zhpe_stats_subid(RECV, 0));
+				zhpe_stats_start(zhpe_stats_subid(RECV, 0));
 				zhpe_pe_rx_handle_send(conn, zhdr);
-				zhpe_stats_stop(zpe_stats_subid(RECV, 0));
+				zhpe_stats_stop(zhpe_stats_subid(RECV, 0));
 				break;
 
 			case ZHPE_OP_STATUS:
